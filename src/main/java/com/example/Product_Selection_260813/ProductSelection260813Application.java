7b4bd0338f92eb2e3b2595c34c8328bac4bff306
tiny9745 <87311498+tiny9745@users.noreply.github.com>
2026-08-13 16:58:29 +0800

    預設專案資料

---
+++ src/main/java/com/example/Product_Selection_260813/ProductSelection260813Application.java
@@ -0,0 +1,13 @@
+package com.example.Product_Selection_260813;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSelection260813Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSelection260813Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Product_Selection_260813/ProductSelection260813ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Product_Selection_260813;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSelection260813ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
